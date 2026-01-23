abf8fbbcdcb0293d424a03606e37484d2ee6e3ed
tanvii-18 <8438tanvipatel@gmail.com>
2026-01-23 09:19:56 +0530

    23-1-26

---
+++ backend/server.js
@@ -0,0 +1,12 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+
+const app = express();
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/");
+
+app.listen(4000, () => {
+  console.log("server started on http://localhost:4000");
+});
