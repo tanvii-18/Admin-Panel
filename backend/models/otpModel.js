33e46763d0399ad413630ea56965ce80d45b2531
tanvii-18 <8438tanvipatel@gmail.com>
2026-01-25 13:40:22 +0530

    25-1-26

--- backend/models/otpModel.js
+++ backend/models/otpModel.js
@@ -1,1 +1,12 @@
 import mongoose from "mongoose";
+
+const otpSchema = new mongoose.Schema(
+  {
+    otp: Number,
+    email: String,
+    expiryAt: Date,
+  },
+  { timestamps: true },
+);
+
+export const otpCollection = mongoose.model("otp", otpSchema);
