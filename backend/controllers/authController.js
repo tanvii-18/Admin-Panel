bf4d14bdc53e7e7bcad5ca0113802b97da731102
tanvii-18 <8438tanvipatel@gmail.com>
2026-01-25 20:20:33 +0530

    25-1-26

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,3 +1,11 @@
 import { AuthCollection } from "../models/authModel";
 
-export const signup = () => {};
+export const signup = (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const hashed = bcrypt.hash(password, 12);
+  } catch (error) {
+    res.json({ status: false, message: "Can't Register User!" });
+    console.log(error);
+  }
+};
