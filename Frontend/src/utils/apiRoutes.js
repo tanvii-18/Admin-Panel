998991c9459ba9ee15d0b742800f62c8ba9cfc92
tanvii-18 <8438tanvipatel@gmail.com>
2026-01-30 21:15:10 +0530

    30-1-26

---
+++ Frontend/src/utils/apiRoutes.js
@@ -0,0 +1,1 @@
+export const api_auth = "http://localhost:5173/api/auth";
--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ import cors from "cors";
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: "http://localhost:5173/", credentials: true }));
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 
 connectDB();
 
