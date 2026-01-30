79fcb1b81b8b9c9baaafc0dde39713f07b9e38b6
tanvii-18 <8438tanvipatel@gmail.com>
2026-01-30 09:20:29 +0530

    30-1-26

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -3,6 +3,10 @@ import { User } from "../models/userModel.js";
 import bcrypt from "bcrypt";
 import { sendOTP } from "../services/otp_services.js";
 import { otpCollection } from "../models/otpModel.js";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 export const signup = async (req, res) => {
   const { email, password } = req.body;
@@ -69,10 +73,14 @@ export const verifyOtp = async (req, res) => {
   try {
     const otpRecord = await otpCollection.findOne({ email, otp });
 
-    const isOtpMatched = otpCollection.compare(otp, otpRecord.otp);
+    // comparing otp with database otp
 
-    if (!isOtpMatched) {
-      return res.status(400).json({ status: false, message: "Incorrect OTP!" });
+    if (!otpRecord) {
+      return res.status(400).json({ status: false, message: "OTP not found." });
+    }
+
+    if (otpRecord.otp !== otp) {
+      return res.status(400).json({ status: false, message: "Incorrect OTP." });
     }
 
     if (otpRecord.expiryAt < new Date(Date.now())) {
@@ -81,15 +89,62 @@ export const verifyOtp = async (req, res) => {
 
     await otpCollection.deleteMany({ email });
 
-    res.status(200).json({
+    const user = await AuthCollection.findOne({ email });
+
+    const token = jwt.sign(
+      { id: user._id, email: user.email },
+      process.env.SECRET_KEY,
+      {
+        expiresIn: "1d",
+      },
+    );
+    res.cookie("auth_token", token, {
+      maxAge: 1000 * 60 * 60 * 24,
+      sameSite: "strict",
+      httpOnly: true,
+    });
+
+    res.json({
       status: true,
-      message: "OTP verified! User LogIn Successfully.",
+      message: "OTP is verified & Signin successfully !",
     });
   } catch (error) {
-    return res.status(500).json({
-      status: false,
-      message: "OTP verification Failed.",
-      error: error.message,
-    });
+    console.log("Catch block", error);
+
+    return res
+      .status(500)
+      .json({ status: false, message: "OTP Verification Failed!" });
   }
 };
+
+// export const verifyOtp = async (req, res) => {
+//   const { email, otp } = req.body;
+
+//   try {
+//     const otpRecord = await otpCollection.findOne({ email });
+
+//     const isOtpMatched = await bcrypt.compare(otp, otpRecord.otp);
+
+//     if (!isOtpMatched) {
+//       return res.status(400).json({ status: false, message: "Incorrect OTP!" });
+//     }
+
+//     if (otpRecord.expiryAt < new Date(Date.now())) {
+//       return res.status(400).json({ status: false, message: "OTP expired." });
+//     }
+
+//     await otpCollection.deleteMany({ email });
+
+//     res.status(200).json({
+//       status: true,
+//       message: "OTP verified! User LogIn Successfully.",
+//     });
+//   } catch (error) {
+//     console.log(error);
+//     return res.status(500).json({
+//       status: false,
+//       message: "OTP verification Failed.",
+//       error: error.message,
+//     });
+//   }
+// };
--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,4 +1,4 @@
-import { loginUser, signup } from "../controllers/authController.js";
+import { loginUser, signup, verifyOtp } from "../controllers/authController.js";
 import { signinUser, signupUser } from "../middleware/authMiddleware.js";
 import express from "express";
 
@@ -6,5 +6,6 @@ const auth_Route = express.Router();
 
 auth_Route.post("/signup", signupUser, signup);
 auth_Route.post("/login", signinUser, loginUser);
+auth_Route.post("/verify-otp", verifyOtp);
 
 export default auth_Route;
