322ad0f1f8c1efa99d80fa91621cf2bebc471e3b
tanvii-18 <8438tanvipatel@gmail.com>
2026-01-31 20:59:29 +0530

    31-1-26

--- Frontend/src/screens/SignIn.jsx
+++ Frontend/src/screens/SignIn.jsx
@@ -3,12 +3,17 @@ import { Link } from "react-router";
 
 export default function SignIn() {
   return (
-    <div>
-      <div className="m-2">
-        <h1 className="text-2xl font-bold">LogIn to your account</h1>
-        <span>
-          Don't have account? <Link to="/signup">Sign Up</Link>
-        </span>
+    <div className="min-h-screen flex flex-col items-center justify-center">
+      <div className=" h-[500px] w-[450px] py-5 rounded-lg shadow-lg bg-amber-200 m-md-3">
+        <div className="flex flex-col items-center bg-red-300">
+          <h1 className="text-2xl font-bold">LogIn to your account</h1>
+          <span className="text-[1px] text-gray-500">
+            Don't have account?{" "}
+            <Link to="/signup" className="text-blue-700">
+              Sign Up
+            </Link>
+          </span>
+        </div>
       </div>
     </div>
   );
