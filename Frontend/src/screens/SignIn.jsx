dcc199d6f596059a3bfb6c632a1cf5bea24575a6
tanvii-18 <8438tanvipatel@gmail.com>
2026-02-01 23:24:42 +0530

    1-2-26

--- Frontend/src/screens/SignIn.jsx
+++ Frontend/src/screens/SignIn.jsx
@@ -4,9 +4,11 @@ import { Link } from "react-router";
 export default function SignIn() {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center">
-      <div className=" h-[500px] w-[450px] py-5 rounded-lg shadow-lg bg-amber-200 m-md-3">
-        <div className="flex flex-col items-center bg-red-300">
-          <h1 className="text-2xl font-bold">LogIn to your account</h1>
+      <div className="h-[450px] w-[450px] py-6 rounded-lg shadow-lg ">
+        <div className="flex flex-col items-center">
+          <h1 className="text-2xl font-bold text-gray-800">
+            LogIn to your account
+          </h1>
           <span className="text-[1px] text-gray-500">
             Don't have account?{" "}
             <Link to="/signup" className="text-blue-700">
@@ -14,6 +16,41 @@ export default function SignIn() {
             </Link>
           </span>
         </div>
+
+        <form className="flex flex-col items-center mt-10">
+          <div className="flex flex-col justify-center items-center gap-4">
+            <div className="flex flex-col w-90 gap-2">
+              <label htmlFor="email" className="px-2">
+                Email
+              </label>
+              <input
+                type="email"
+                id="email"
+                placeholder="Email"
+                className="mb-4 px-5 py-3 border border-gray-300 rounded-full text-[14px] bg-[#efefef]"
+              />
+            </div>
+
+            <div className="flex flex-col w-90 gap-2">
+              <label htmlFor="password" className="px-2">
+                Password
+              </label>
+
+              <input
+                type="password"
+                id="password"
+                placeholder="Password"
+                className="mb-4 px-5 py-3 border border-gray-300 rounded-full text-[14px] bg-[#efefef]"
+              />
+            </div>
+          </div>
+          <button
+            type="submit"
+            className="w-90 bg-gray-800 text-white px-4 py-[11px] rounded-full hover:bg-gray-700 my-5 cursor-pointer"
+          >
+            Sign In
+          </button>
+        </form>
       </div>
     </div>
   );
